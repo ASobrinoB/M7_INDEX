680e7ebf57b6980419babb1d859e2aee332a5289
ASobrinoB <telemensajes@hotmail.com>
2024-09-27 17:05:59 -0300

    upgrade vite

--- src/components/Home/ProductList.jsx
+++ src/components/Home/ProductList.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import ProductContext from "../../contexts/products/ProductContext";
-import PaypalButton from "./PayPalButton";
+import PaypalButton from './PayPalButton';
 
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
