302281c1f334692e8aef57392f9a0caecf97e484
ASobrinoB <telemensajes@hotmail.com>
2024-09-27 16:08:03 -0300

    revisando

--- src/components/Home/ProductList.jsx
+++ src/components/Home/ProductList.jsx
@@ -10,7 +10,6 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
-import PaymentIcon from '@mui/icons-material/Payment';
 
 export const ProductList = () => {
 
